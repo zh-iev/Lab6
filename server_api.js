2410d25b7fa93eece29361dea22c787d4cad62bd
zh_iev <71790904+zhiev@users.noreply.github.com>
2021-01-20 20:46:50 +0300

    Add files via upload

---
+++ server_api.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+let storage = [];
+
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/guide', function (req,res) {
+    res.send(storage);
+});
+
+
+app.get('/guide/:id', function (req,res) {
+    const id = req.params.id;
+    const item = storage[id];
+    if (item===null || id>=storage.length) {
+        res.sendStatus(404);
+    } else res.send(storage[id]);
+
+});
+
+app.post('/guide', function (req,res) {
+    const obj = storage.push(req.body)-1;
+    res.send(obj.toString());
+});
+
+app.put('/guide/:id', function (req,res) {
+    const id = req.params.id;
+    storage[id] = req.body;
+    res.send(id.toString());
+});
+
+app.delete('/guide/:id', function (req,res) {
+    const id = req.params.id;
+    storage[id] = null;
+    res.send(id.toString());
+});
+
+app.listen(8080, function() {
+    console.log('Сервер запущен');
+});
